86c9db626646886c373f51d1116da32e7c1edb97
Michael Dreesen <mdreesen@Mac.lan>
2025-01-07 13:46:21 -0700

    feature(change grid)

--- components/charts/UseWeightPerDay.tsx
+++ components/charts/UseWeightPerDay.tsx
@@ -9,9 +9,9 @@ export const UseWeightPerDay = ({ data }: any) => {
     <>
       <ResponsiveContainer width='100%' height={300}>
         <LineChart width={500} height={300} data={data}>
-          <XAxis dataKey="weight_date" stroke="black" />
-          <YAxis dataKey="weight"  stroke="black" />
           <CartesianGrid stroke="white" />
+          <XAxis dataKey="weight_date" stroke="black" />
+          <YAxis />
           <Tooltip />
           <Line name="weight" type="monotone" dataKey="weight" stroke="black" dot={false} />
         </LineChart>
