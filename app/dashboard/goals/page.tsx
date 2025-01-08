f6b1def96c700f860de0cc6d967c8aca7c74caa3
Michael Dreesen <mdreesen@Mac.lan>
2025-01-08 12:00:43 -0700

    fix(change a tag to link tag)

--- app/dashboard/goals/page.tsx
+++ app/dashboard/goals/page.tsx
@@ -37,12 +37,6 @@ export default async function Page() {
                             </div>
                         </div>
                         <div className="flex flex-none items-center gap-x-4">
-                            <a
-                                href={item.href}
-                                className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
-                            >
-                                View item<span className="sr-only">, {item.name}</span>
-                            </a>
                             <Menu as="div" className="relative flex-none">
                                 <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
                                     <span className="sr-only">Open options</span>
@@ -53,12 +47,12 @@ export default async function Page() {
                                     className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                                 >
                                     <MenuItem>
-                                        <a
+                                        <Link
                                             href={`/dashboard/goals/edit/${JSON.parse(JSON.stringify(item._id))}`}
                                             className="block px-3 py-1 text-sm/6 text-gray-900 data-[focus]:bg-gray-50 data-[focus]:outline-none"
                                         >
                                             Edit<span className="sr-only">, {item.name}</span>
-                                        </a>
+                                        </Link>
                                     </MenuItem>
                                     <MenuItem>
                                         <ButtonDeleteGoal data={JSON.parse(JSON.stringify(item._id))} />
