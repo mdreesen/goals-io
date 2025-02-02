8f5ca087154142a86935bee817c4401c4f4a3981
Michael Dreesen <mdreesen@Mac.lan>
2025-02-01 22:11:14 -0700

    feature(mobile nav bottom border)

--- components/navigation/NavigationPhone.tsx
+++ components/navigation/NavigationPhone.tsx
@@ -1,69 +1,67 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export default async function NavigationPhone() {
+export default function NavigationPhone() {
+    const pathName = usePathname();
+
+    const links = [
+        {
+            id: 1,
+            href: "/dashboard/body",
+            alt: "Body icon",
+            assetSrc: '/assets/icons/body.png',
+            style: 'h-[3rem] w-[auto] object-cover scale-75'
+        },
+        {
+            id: 2,
+            href: "/dashboard/mind",
+            alt: "Mind icon",
+            assetSrc: '/assets/icons/mind.png',
+            style: 'h-[3rem] w-[auto] object-cover scale-75'
+        },
+        {
+            id: 3,
+            href: "/dashboard",
+            alt: "Ascend logo back to dashboard",
+            assetSrc: '/assets/logo_transparent_512x512.png',
+            style: 'h-[4rem] w-[auto] object-cover scale-75 bg-gray-900 rounded-full'
+        },
+        {
+            id: 4,
+            href: "/dashboard/spirit",
+            alt: "Soul icon",
+            assetSrc: '/assets/icons/soul.png',
+            style: 'h-[3rem] w-[auto] object-cover scale-75'
+        },
+        {
+            id: 5,
+            href: "/dashboard/profile",
+            alt: "Profile icon",
+            assetSrc: '/assets/icons/profile.png',
+            style: 'h-[3rem] w-[auto] object-cover scale-75'
+        },
+    ]
 
     return (
         <nav className="bg-white sm:block md:hidden lg:hidden h-[6rem] fixed bottom-0 w-full z-50">
             <ul className='flex justify-around h-[100%] content-center items-center'>
-                <Link href={'/dashboard/body'}>
-                    <li>
-                        <Image
-                            alt="Body icon"
-                            width={100}
-                            height={100}
-                            src={'/assets/icons/body.png'}
-                            className="h-[3rem] w-[auto] object-cover scale-75"
-                        />
-                    </li>
-                </Link>
-                <Link href={'/dashboard/mind'}>
-                    <li>
-                        <Image
-                            alt="Mind icon"
-                            width={100}
-                            height={100}
-                            src={'/assets/icons/mind.png'}
-                            className="h-[3rem] w-[auto] object-cover scale-75"
-                        />
-                    </li>
-                </Link>
+                {links.map((item) => (
+                    <Link href={item.href} key={item.id}>
+                        <li>
+                            <Image
+                                alt={item.alt}
+                                width={100}
+                                height={100}
+                                src={item.assetSrc}
+                                className={`${item.style}`}
+                            />
 
-                <Link href={'/dashboard'}>
-                    <li>
-                        <Image
-                            alt="Ascend logo back to dashboard"
-                            width={100}
-                            height={100}
-                            src={'/assets/logo_transparent_512x512.png'}
-                            className="h-[4rem] w-[auto] object-cover scale-75 bg-gray-900 rounded-full"
-                        />
-                    </li>
-                </Link>
-
-                <Link href={'/dashboard/spirit'}>
-                    <li>
-                        <Image
-                            alt="Soul icon"
-                            width={100}
-                            height={100}
-                            src={'/assets/icons/soul.png'}
-                            className="h-[3rem] w-[auto] object-cover scale-75"
-                        />
-                    </li>
-                </Link>
-                <Link href={'/dashboard/profile'}>
-                    <li>
-                        <Image
-                            alt="Profile icon"
-                            width={100}
-                            height={100}
-                            src={'/assets/icons/profile.png'}
-                            className="h-[3rem] w-[auto] object-cover scale-75"
-                        />
-                    </li>
-                </Link>
-
+                            <div className={`${pathName === item.href && 'border-b-2 border-indigo-900'}`}/>
+                        </li>
+                    </Link>
+                ))}
             </ul>
         </nav>
     )
