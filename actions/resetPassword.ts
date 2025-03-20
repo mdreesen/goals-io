cc75a271ab100b7ac99f8a76c6403230d6add09f
Michael Dreesen <mdreesen@Mac.lan>
2025-03-20 11:46:02 -0600

    Cleanup(Taking out mailtrap and taking out irrelevent keys)

--- actions/resetPassword.ts
+++ actions/resetPassword.ts
@@ -1,5 +1,4 @@
 "use server"
-import nodemailer from "nodemailer";
 import { nanoid } from "nanoid";
 import { connectDB } from "@/lib/mongodb";
 import { Resend } from 'resend';
@@ -12,16 +11,6 @@ export const resetPassword = async (values: any) => {
 
     // Nanoid creates a unique string
     const token = nanoid(32);
-
-    // const transporter = nodemailer.createTransport({
-    //     host: `${process.env.MAILTRAP_HOST}`,
-    //     port: Number(process.env.MAILTRAP_PORT),
-    //     secure: false, // upgrade later with STARTTLS
-    //     auth: {
-    //         user: `${process.env.MAILTRAP_USERNAME}`,
-    //         pass: `${process.env.MAILTRAP_PASSWORD}`,
-    //     },
-    // });
 
     const htmlBody = `
     <div>
@@ -46,14 +35,7 @@ export const resetPassword = async (values: any) => {
                     error: 'Email not found'
                 }
             }
-    
-            // const info = await transporter.sendMail({
-            //     from: 'NoReply@ascendpod.com', // sender address
-            //     to: email, // list of receivers
-            //     subject: "Reset your password", // Subject line
-            //     text: "Reset your password", // plain text body
-            //     html: htmlBody, // html body
-            // });
+
             const resend = new Resend(`${process.env.RESEND_KEY}`);
 
             await resend.emails.send({
@@ -63,8 +45,6 @@ export const resetPassword = async (values: any) => {
                 html: htmlBody
               });
     
-            // console.log("Message sent: %s", info.messageId);
-            // console.log(`Updating ${email} with ${token}`)
             await User.findOneAndUpdate({ email: email }, { resetPasswordToken: token })
         }
 
