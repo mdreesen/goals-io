e5d6825456a9a08d93ee885a56d72c917afd7281
Michael Dreesen <mdreesen@Mac-1566.lan>
2025-04-25 09:43:06 -0600

    feature(change yarn to npm | update react and react-dom)

--- app/dashboard/loading.tsx
+++
@@ -1,6 +0,0 @@
-import LoadingScale from "@/components/loaders/LoadingScale";
-
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <LoadingScale />
-  }
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="ltr" suppressHydrationWarning>
+    <html lang="en" dir="ltr">
       <NetworkStatusProvider>
         <Provider>
           <body>
